2c84141c96ec103eb86a83269b44f41ffe1951c2
Nicky <122755801+selfnicky@users.noreply.github.com>
2024-11-29 03:03:07 +0100

    Update backend.tf

--- terraform-remote-backend/backend.tf
+++ terraform-remote-backend/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "week6-self-buckethghgffddfghjjgf-terraform"
+    bucket = ""
     key    = "dev/terraform.tfstate"
     region = "us-east-1"
-    dynamodb_table = "terraform-statefile-lockgfddffdssd"
+    dynamodb_table = ""
     encrypt = true
   }
 }
